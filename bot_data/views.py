eeced82d7f9e544d072111c2eb637531bebc5551
Fly-Potato <live1006186250@live.com>
2021-02-27 18:16:12 +0800

    update

--- bot_data/views.py
+++ bot_data/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse, Http404, HttpRequest
+from django.views import View
 
-# Create your views here.
+
+def index():
+    pass
+
+
+class Material(View):
+    def get(self, request: HttpRequest):
+        pass
+
+    def post(self, request: HttpRequest):
+        pass
