a736c4d6ae3af78dbc2f6e1a7d413ae66cfe6fd7
Fly-Potato <live1006186250@live.com>
2021-02-27 16:58:43 +0800

    update settings.py

--- data_center/settings.py
+++ data_center/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = ')j%g_mc!wir7zo2^v4hvwqvt5&-@_9w2(&t&fobhse5z2ll38t'
+SECRET_KEY = ')j%g_mc!wir7zo2^v4hvwqvt5&-@_9w2(&t&fobhse5z2ll38t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
