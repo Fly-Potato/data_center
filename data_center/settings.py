47d79e6b712a47553f61e84653efc0bb999ed064
Fly-Potato <live1006186250@live.com>
2021-03-01 15:19:51 +0800

    Update settings.py

--- data_center/settings.py
+++ data_center/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'data_center.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'data_center',
+        'USER': 'root',
+        'PASSWORD': 'tudou123',
     }
 }
 
@@ -105,15 +107,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
