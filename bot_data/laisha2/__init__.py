df78c33c495bbcf89c341144f82e467f8046e79d
Fly-Potato <live1006186250@live.com>
2021-03-03 15:21:21 +0800

    Update __init__.py

--- bot_data/laisha2/__init__.py
+++ bot_data/laisha2/__init__.py
@@ -17,8 +17,11 @@ class Material(View):
         if name:
             materials = MaterialModel.objects.filter(name__contains=name)
             if materials:
+                res = list()
                 for material in materials:
-                    print(model_to_dict(material))
+                    res.append(model_to_dict(material))
+                return JsonResponse(dict(code=1, materials=res))
+
             else:
                 return JsonResponse(dict(code=0, info='未查找到相关材料！'))
         return HttpResponse('get')
@@ -42,19 +45,20 @@ class Material(View):
                     info_list = line.split(' ')
                     if len(info_list) >= 6:
                         try:
-                            MaterialModel.objects.get(name=info_list[0])
+                            material_model = MaterialModel.objects.get(name=info_list[0])
                         except MaterialModel.DoesNotExist:
                             material_model = MaterialModel()
-                        else:
-                            continue
                         material_model.name = info_list[0]
                         material_model.level = info_list[1]
                         material_model.attribute = info_list[2]
                         material_model.attribute_num = info_list[3]
                         material_model.type = info_list[4]
-                        addrs = []
-                        for addr in info_list[5:]:
-                            addrs += addr + ' '
+                        addrs = ''
+                        if not len(info_list[5:]) == 1:
+                            for addr in info_list[5:]:
+                                addrs += addr + ' '
+                        else:
+                            addrs = info_list[5]
                         material_model.addr = addrs
                         try:
                             material_model.save()
