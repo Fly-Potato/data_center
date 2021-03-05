709342d5dc46978d27a979593d25ee50cacf335d
Fly-Potato <live1006186250@live.com>
2021-03-05 17:25:08 +0800

    add jx3

--- bot_data/urls.py
+++ bot_data/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .laisha2 import urls as laisha2_url
+from .jx3 import urls as jx3_url
 from .views import index
 
 urlpatterns = [
     path('', index),
-    path('laisha2/', include(laisha2_url))
+    path('laisha2/', include(laisha2_url)),
+    path('jx3/', include(jx3_url))
 ]
